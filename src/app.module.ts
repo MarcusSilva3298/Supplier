244e925557254ce356bc35461c67bec1b9bd0268
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-01-21 11:09:07 -0300

    Feature: Hello World

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
 import { UsersModule } from './users/users.module'
 
 @Module({
   imports: [UsersModule],
-  controllers: [],
+  controllers: [AppController],
   providers: []
 })
 export class AppModule {}
